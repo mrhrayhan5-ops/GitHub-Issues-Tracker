c78599994b95bc377903469dfc66d09f119c97f2
mrhrayhan5-ops <mrhrayhan5@gmail.com>
2026-03-14 20:58:32 +0600

    fixign bug

--- script/signin.js
+++ script/signin.js
@@ -1,17 +1,17 @@
 // sign in function
 
-document.getElementById("signin-btn").addEventListener("click", () =>{
+document.getElementById("signin-btn").addEventListener("click", () => {
     const inputName = document.getElementById("input-name");
     const username = inputName.value;
-    
+
     const inputPassword = document.getElementById("input-password");
     const password = inputPassword.value;
-    
+
     // console.log(window.location);
-    if(username === "admin" && password === "admin123"){
+    if (username === "admin" && password === "admin123") {
         alert("Sign In Successful!");
         window.location.assign("/main.html");
-    }else{
+    } else {
         alert("Sign In Unsuccessful!");
         return;
     }
